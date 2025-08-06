9fe28c7eb23327c68685f5779005159180fa25a4
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-06 10:09:34 +0000

    sdsd

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,18 @@
+/*
+ * File: 6-size.c
+ * Auth: [Your Name]
+ * Desc: C program that prints the size of various data types
+ */
+
 #include <stdio.h>
 
-int main(void)
-{
-	printf("%lu\n", sizeof(...));
-	return (0);
-}
 /**
- * main - Entry point of the program
- *
- * Description: This program prints the line:
- * "Size",
- * followed by a new line. It uses the puts function and
- * returns 0 upon successful completion.
+ * main - Prints the sizes of various C data types
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	printf("%d\n", size);
+	return (0);
+}
