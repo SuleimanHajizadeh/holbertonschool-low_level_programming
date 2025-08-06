a61cba3d123fc6b6f47f7a3699c33690444901e2
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-06 10:00:40 +0000

    sdsd

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf($size);
+	return (0);
+}
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the line:
+ * "Size",
+ * followed by a new line. It uses the puts function and
+ * returns 0 upon successful completion.
+ *
+ * Return: Always 0 (Success)
+ */
