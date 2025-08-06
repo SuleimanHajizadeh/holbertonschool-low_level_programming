5d6c214a56155e20b23413935d25dfa3e64f4d31
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-06 10:28:10 +0000

    task

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,6 +13,15 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof (...));
+	int 1
+	long int 12
+	long long int 123
+	char "Suleyman"
+	float 1.5
+	printf("Size of an int: %lu byte(s)\n", sizeof (1));
+	printf("Size of long int: %lu byte(s)\n", sizeof (12));
+	printf("Size of long long int: %lu byte(s)\n", sizeof (123));
+	printf("Size of char: %lu byte(s)\n", sizeof ("Suleyman"));
+	printf("Size of float: %lu byte(s)\n", sizeof (1.5));
 	return (0);
 }
