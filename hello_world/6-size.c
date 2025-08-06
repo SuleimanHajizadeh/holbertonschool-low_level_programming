ea82b669123e391e0f56e0b163a7f8a60ec2ef7a
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-06 10:42:22 +0000

    task

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long int tvelve;
 	long long int one_hundred_tvelve;
 	float one_point_five;
-	printf("Size of an int: %lu byte(s)\n", sizeof (Suleyman));
-	printf("Size of long int: %lu byte(s)\n", sizeof (one));
-	printf("Size of long long int: %lu byte(s)\n", sizeof (tvelve));
-	printf("Size of char: %lu byte(s)\n", sizeof (one_hundred_tvelve));
-	printf("Size of float: %lu byte(s)\n", sizeof (one_point_five));
+	printf("Size of a char: %lu byte(s)\n", sizeof (Suleyman));
+	printf("Size of an int: %lu byte(s)\n", sizeof (one));
+	printf("Size of a long int: %lu byte(s)\n", sizeof (tvelve));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof (one_hundred_tvelve));
+	printf("Size of a float: %lu byte(s)\n", sizeof (one_point_five));
 	return (0);
 }
