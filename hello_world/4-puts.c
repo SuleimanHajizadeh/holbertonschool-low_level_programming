2a8c6cd4b76ead94f3a53a5164cd2181325083e4
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-06 09:46:18 +0000

    sdsd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 /**
  * main - Entry point of the program
  *
@@ -15,3 +10,8 @@ int main(void)
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
