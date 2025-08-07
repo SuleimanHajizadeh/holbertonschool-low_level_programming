5afb8896bb8bef8218bbe327a044575fcc3e907e
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-07 08:59:47 +0000

    sdsds

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, excluding q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
