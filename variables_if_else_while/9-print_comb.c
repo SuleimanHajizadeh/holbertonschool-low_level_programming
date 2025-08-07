e469a6783901597b5066ec234e8f45e21923bd74
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-07 09:25:07 +0000

    sdsds

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,20 +10,20 @@
  */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < 10)
-    {
-        putchar(i + '0');
+	while (i < 10)
+	{
+		putchar(i + '0');
 
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        i++;
-    }
-    putchar('\n');
-    
-    return (0);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
 }
