f51285264c18ee8ff4c4102b2b0b73fcefdf45b0
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-07 09:22:41 +0000

    sdsd

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers from 0 to 9
+ *              separated by ", " using only putchar and no char variables
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        putchar(i + '0');
+
+        if (i != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        i++;
+    }
+    putchar('\n');
+    
+    return (0);
+}
