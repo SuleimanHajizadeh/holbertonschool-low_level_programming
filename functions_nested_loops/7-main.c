a92adf0426d4d2a20c8d69a47afdb92aae3d4e34
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-08 10:22:49 +0000

    sdsdsd

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -3,13 +3,16 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	print_last_digit(98);
-	print_last_digit(0);
-	print_last_digit(-1024);
-	_putchar('\n');
-	return (0);
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
