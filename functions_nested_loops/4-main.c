a30204f3d17ffd5a2da87ec712533f858e0e0973
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-08 09:58:30 +0000

    sdsd

---
+++ functions_nested_loops/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	printf("%d\n", r);
+	r = _isalpha('o');
+	printf("%d\n", r);
+	r = _isalpha(108);
+	printf("%d\n", r);
+	r = _isalpha(';');
+	printf("%d\n", r);
+	return (0);
+}
