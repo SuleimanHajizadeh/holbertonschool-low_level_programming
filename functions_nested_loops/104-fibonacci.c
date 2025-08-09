68cd93a6cd7607bd61cb6f4bff27ca07dc2c2c31
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-09 17:56:55 +0000

    sdsd

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space.
+ * Return: Always 0.
+ */
 int main(void)
 {
+	int count;
 	unsigned long fib1_h = 0, fib1_l = 1;
 	unsigned long fib2_h = 0, fib2_l = 2;
 	unsigned long next_h, next_l;
-	int count;
 
 	printf("%lu, %lu", fib1_l, fib2_l);
 
