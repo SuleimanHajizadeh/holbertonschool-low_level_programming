60c061115e1f01e5d21b28da374dc2f05e346b6d
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-09 16:04:01 +0000

    sdsd

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Description: This function prints the multiplication table for
+ * numbers 0 through 9, with proper formatting so columns align.
+ */
+void times_table(void)
+{
+    int a, b, timestable_variable;
+
+    for (a = 0; a <= 9; a++)
+    {
+        for (b = 0; b <= 9; b++)
+        {
+            timestable_variable = a * b;
+
+            if (b == 0)
+            {
+                _putchar(timestable_variable + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (timestable_variable < 10)
+                    _putchar(' ');
+                else
+                    _putchar((timestable_variable / 10) + '0');
+
+                _putchar((timestable_variable % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
