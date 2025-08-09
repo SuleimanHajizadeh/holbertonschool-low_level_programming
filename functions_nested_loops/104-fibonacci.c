c614fbe9a9834de88893d4945b6b15c2b8e85239
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-09 18:01:00 +0000

    sdsd

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -3,34 +3,51 @@
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	int count;
-	unsigned long fib1_h = 0, fib1_l = 1;
-	unsigned long fib2_h = 0, fib2_l = 2;
-	unsigned long next_h, next_l;
+	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	printf("%lu, %lu", fib1_l, fib2_l);
-
-	for (count = 3; count <= 98; count++)
+	/* İlk 92 Fibonacci ədədi (64-bit dəyişənlərdə rahat saxlanır) */
+	printf("%lu, %lu", fib1, fib2);
+	for (count = 3; count <= 92; count++)
 	{
-		next_l = fib1_l + fib2_l;
-		next_h = fib1_h + fib2_h;
-		if (next_l < fib1_l)
-			next_h += 1;
+		sum = fib1 + fib2;
+		printf(", %lu", sum);
+		fib1 = fib2;
+		fib2 = sum;
+	}
 
-		if (next_h == 0)
-			printf(", %lu", next_l);
-		else
-			printf(", %lu%09lu", next_h, next_l);
+	/* Böyük Fibonacci ədədləri üçün ədədi hissələrə ayır */
+	fib1_half1 = fib1 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
-		fib1_l = fib2_l;
-		fib1_h = fib2_h;
-		fib2_l = next_l;
-		fib2_h = next_h;
+	/* Sonrakı 6 Fibonacci ədədini iki hissə kimi hesabla */
+	for (count = 93; count <= 98; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf(", %lu%09lu", half1, half2);
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 	printf("\n");
+
 	return (0);
 }
