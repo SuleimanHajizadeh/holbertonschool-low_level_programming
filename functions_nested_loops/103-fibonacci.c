77c0afd27d60a3d9977b6207af92875fbe86bbce
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-09 17:10:24 +0000

    sdsd

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,21 +8,22 @@
  */
 int main(void)
 {
-    unsigned long fib1 = 1, fib2 = 2, next;
-    unsigned long sum = 2;  /* 2 is even, starts sum from 2 */
+	unsigned long fib1 = 1, fib2 = 2, next;
+	unsigned long sum = 2;	/* 2 is even, starts sum from 2 */
 
-    while (1)
-    {
-        next = fib1 + fib2;
-        if (next > 4000000)
-            break;
-        if (next % 2 == 0)
-            sum += next;
-        fib1 = fib2;
-        fib2 = next;
-    }
+	while (1)
+	{
+		next = fib1 + fib2;
+		if (next > 4000000)
+			break;
+		if (next % 2 == 0)
+			sum += next;
+		fib1 = fib2;
+		fib2 = next;
+	}
 
-    printf("%lu\n", sum);
+	printf("%lu\n", sum);
 
-    return (0);
+	return (0);
 }
+
