4ce4cf604cf09d26e50c4669edf54805006596a6
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-12 09:12:22 +0000

    dsdsd

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,11 +4,11 @@
  * swap_int - Write a function that swaps the values of two integers.
  *
  * &a and &b: change the values of a and b.
+ *
  */
 void swap_int(int *a, int *b)
 {
 	int address;
-
 	address = *a;
 	*a = *b;
 	*b = address;
