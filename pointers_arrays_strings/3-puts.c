55bf78e2820065ae245b01a3c0dc85d4b4f72820
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-12 11:50:49 +0000

    sdsds

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - prints a string followed by a new line to stdout
- * @str: pointer to the string to print
+ *@str: pointer to the string to print
  */
 void _puts(char *str)
 {
-    int i = 0;
+   	 int i = 0;
 
-    while (str[i] != '\0')
+   	 while (str[i] != '\0')
     {
         _putchar(str[i]);
         i++;
