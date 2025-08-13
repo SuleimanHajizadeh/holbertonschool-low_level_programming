07e14067d092c2170fa0b5a7c8d1b22ac1ba5f10
SuleimanHajizadeh <suleyman.hacizade1@gmai.com>
2025-08-13 17:03:40 +0000

    sdsd

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,29 +3,21 @@
 #include <time.h>
 
 /**
- * main - generates a random valid password for 101-crackme
+ * main - generates a printable random valid password for 101-crackme
  *
  * Return: 0 on success
- *
- * Idea:
- *  - The crackme accepts any sequence of bytes whose sum is 2772.
- *  - We keep emitting random bytes (0..127) while keeping the running sum ≤ 2772.
- *  - The last byte is chosen so that the total sum is exactly 2772.
  */
 int main(void)
 {
-	int sum = 0;
-	int byte;
+	int sum = 0, byte;
 
 	srand((unsigned int)time(NULL));
 
-	/* Keep adding random bytes as long as we don't overshoot 2772 */
 	while (sum < 2772)
 	{
-		/* Random byte in [0, 127] */
-		byte = rand() % 128;
+		/* printable ASCII range: 33 (!) to 126 (~) */
+		byte = (rand() % 94) + 33;
 
-		/* If this exact byte would overshoot, force the exact remainder and finish */
 		if (sum + byte > 2772)
 			byte = 2772 - sum;
 
