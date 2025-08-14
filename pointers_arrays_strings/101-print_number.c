6724829bd961dd8fe0eaaf043a54733a37688945
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-14 14:54:32 +0000

    sdsd

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		/* Convert to positive safely */
+		if (n / 10 != 0)
+			print_number(-(n / 10));
+		_putchar('0' + -(n % 10));
+	}
+	else
+	{
+		if (n / 10 != 0)
+			print_number(n / 10);
+		_putchar('0' + (n % 10));
+	}
+}
