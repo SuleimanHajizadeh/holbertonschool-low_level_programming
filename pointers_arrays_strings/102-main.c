f7b1e9f6202954b7ab5b963d369de52a679f2cf9
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-14 15:09:20 +0000

    Sdsd

--- pointers_arrays_strings/102-main.c
+++ pointers_arrays_strings/102-main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    int a[5];
-    int *p;
-    a[0] = 98;
-    a[1] = 198;
-    a[2] = 298;
-    a[3] = 398;
-    a[4] = 498;
-    p = a + 2;
+    int b[5];
+    int *c;
+    b[0] = 98;
+    b[1] = 198;
+    b[2] = 298;
+    b[3] = 398;
+    b[4] = 498;
+    c = b + 2;
     /* Your line of code goes here (line 19) */
     ;
-    printf("a[2] = %d\n", a[2]);
+    printf("b[2] = %d\n", b[2]);
     return (0);
 }
