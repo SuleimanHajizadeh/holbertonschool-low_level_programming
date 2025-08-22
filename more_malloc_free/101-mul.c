0bf298b1ebb6b324f2aec5d6316aa103e50e34b0
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2025-08-22 11:16:29 +0000

    sdsd

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,150 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int _isdigit(char *s);
+int _strlen(char *s);
+void _error(int *res);
+void multiply(char *num1, char *num2);
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
- *
- * Return: void
- */
-void _print(char *str, int l)
-{
-	int i, j;
-
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	free(str);
-}
-
-/**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- *
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
-}
-
-/**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- *
- * Return: 0 if digits, 1 if not
- */
-int check_for_digits(char **av)
-{
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * init - initializes a string
- * @str: sting to initialize
- * @l: length of strinf
- *
- * Return: void
- */
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
+ * main - multiplies two positive numbers
+ * @argc: argument count
  * @argv: argument vector
  *
- * Return: zero, or exit status of 98 if failure
+ * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+		_error(NULL);
 
-	if (argc != 3 || check_for_digits(argv))
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
-	}
-	_print(a, ln - 1);
+	multiply(argv[1], argv[2]);
 	return (0);
+}
+
+/**
+ * _isdigit - checks if a string is composed only of digits
+ * @s: string
+ *
+ * Return: 1 if all digits, 0 otherwise
+ */
+int _isdigit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns string length
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _error - prints Error and exits with status 98
+ * @res: pointer to allocated array, NULL if none
+ */
+void _error(int *res)
+{
+	if (res)
+		free(res);
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * multiply - multiplies two numbers given as strings
+ * @num1: first number
+ * @num2: second number
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1 = _strlen(num1), len2 = _strlen(num2);
+	int *res, i, j, carry, n1, n2, sum, start;
+
+	res = calloc(len1 + len2, sizeof(int));
+	if (!res)
+		_error(NULL);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		n1 = num1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + res[i + j + 1] + carry;
+			carry = sum / 10;
+			res[i + j + 1] = sum % 10;
+		}
+		res[i + j + 1] += carry;
+	}
+
+	start = 0;
+	while (start < (len1 + len2) && res[start] == 0)
+		start++;
+
+	if (start == len1 + len2)
+		_putchar('0');
+	else
+		for (; start < len1 + len2; start++)
+			_putchar(res[start] + '0');
+
+	_putchar('\n');
+	free(res);
 }
