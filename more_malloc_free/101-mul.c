882e52f882a48f59a438979a3b5e6ea02b665f1b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-22 11:17:50 +0400

    sddssdds

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,121 +1,110 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+int _isdigit(char *s);
+void _error(void);
+int _strlen(char *s);
+void multiply(char *num1, char *num2);
 
 /**
- * is_digit - checks if a string contains only digits
- * @s: string to check
- * Return: 1 if all digits, 0 otherwise
- */
-int is_digit(char *s)
-{
-    int i = 0;
-
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to measure
- * Return: length of string
- */
-int _strlen(char *s)
-{
-    int len = 0;
-
-    while (s[len])
-        len++;
-    return (len);
-}
-
-/**
- * multiply - multiplies two positive numbers
- * @num1: first number as string
- * @num2: second number as string
- */
-void multiply(char *num1, char *num2)
-{
-    int len1 = _strlen(num1);
-    int len2 = _strlen(num2);
-    int total_len = len1 + len2;
-    int i, j, carry, product;
-    char *result;
-
-    /* Allocate memory for result (plus null terminator) */
-    result = malloc(sizeof(char) * (total_len + 1));
-    if (result == NULL)
-        exit(98);
-
-    /* Initialize result with zeros */
-    for (i = 0; i < total_len; i++)
-        result[i] = '0';
-    result[total_len] = '\0';
-
-    /* Perform multiplication */
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            product = (num1[i] - '0') * (num2[j] - '0');
-            product += (result[i + j + 1] - '0') + carry;
-            carry = product / 10;
-            result[i + j + 1] = (product % 10) + '0';
-        }
-        result[i] += carry;
-    }
-
-    /* Skip leading zeros */
-    i = 0;
-    while (result[i] == '0' && result[i + 1] != '\0')
-        i++;
-
-    print_number(result + i);
-    free(result);
-}
-
-/**
- * print_number - prints a number string
- * @num: number string to print
- */
-void print_number(char *num)
-{
-    int i = 0;
-
-    while (num[i])
-    {
-        _putchar(num[i]);
-        i++;
-    }
-    _putchar('\n');
-}
-
-/**
- * main - entry point
+ * main - multiplies two positive numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success, 98 on error
+ *
+ * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        print_number("Error");
-        exit(98);
-    }
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+		_error();
 
-    if (!is_digit(argv[1]) || !is_digit(argv[2]))
-    {
-        print_number("Error");
-        exit(98);
-    }
+	multiply(argv[1], argv[2]);
+	return (0);
+}
 
-    multiply(argv[1], argv[2]);
+/**
+ * _isdigit - checks if a string is composed only of digits
+ * @s: string
+ *
+ * Return: 1 if all digits, 0 otherwise
+ */
+int _isdigit(char *s)
+{
+	int i = 0;
 
-    return (0);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns string length
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _error - prints error and exits with status 98
+ */
+void _error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * multiply - multiplies two numbers given as strings
+ * @num1: first number
+ * @num2: second number
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1 = _strlen(num1), len2 = _strlen(num2);
+	int *res, i, j, carry, n1, n2, sum, start;
+
+	res = calloc(len1 + len2, sizeof(int));
+	if (!res)
+		exit(98);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		n1 = num1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + res[i + j + 1] + carry;
+			carry = sum / 10;
+			res[i + j + 1] = sum % 10;
+		}
+		res[i + j + 1] += carry;
+	}
+
+	start = 0;
+	while (start < (len1 + len2) && res[start] == 0)
+		start++;
+
+	if (start == len1 + len2)
+		_putchar('0');
+	else
+	{
+		for (; start < len1 + len2; start++)
+			_putchar(res[start] + '0');
+	}
+	_putchar('\n');
+	free(res);
 }
