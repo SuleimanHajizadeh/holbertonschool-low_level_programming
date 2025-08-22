3bf1ca9c99f1a7437522754a7a11ae6769a0e35b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-22 16:18:28 +0400

    task

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -12,59 +12,63 @@ void multiply(char *num1, char *num2);
  * main - multiplies two positive numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 on success
+ *
+ * Return: 0 on success, exits with 98 on error
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
-        _error(NULL);
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+		_error(NULL);
 
-    multiply(argv[1], argv[2]);
-    return (0);
+	multiply(argv[1], argv[2]);
+	return (0);
 }
 
 /**
- * _isdigit - checks if string contains only digits
- * @s: string
- * Return: 1 if all digits, 0 otherwise
+ * _isdigit - checks if a string contains only digits
+ * @s: string to check
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
  */
 int _isdigit(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
- * _strlen - returns string length
- * @s: string
- * Return: length of string
+ * _strlen - returns the length of a string
+ * @s: string to measure
+ *
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-        i++;
-    return (i);
+	while (s[i])
+		i++;
+	return (i);
 }
 
 /**
- * _error - prints Error and exits
- * @res: pointer to allocated memory, NULL if none
+ * _error - prints "Error" and exits with status 98
+ * @res: pointer to allocated memory to free, NULL if none
  */
 void _error(int *res)
 {
-    if (res)
-        free(res);
-    printf("Error\n");
-    exit(98);
+	if (res)
+		free(res);
+
+	printf("Error\n");
+	exit(98);
 }
 
 /**
@@ -74,43 +78,47 @@ void _error(int *res)
  */
 void multiply(char *num1, char *num2)
 {
-    int len1 = _strlen(num1), len2 = _strlen(num2);
-    int *res;
-    int i, j, carry, n1, n2, sum, start;
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int *res;
+	int i, j, carry, n1, n2, sum, start;
 
-    res = malloc((len1 + len2) * sizeof(int));
-    if (!res)
-        _error(NULL);
+	res = calloc(len1 + len2, sizeof(int));
+	if (!res)
+		_error(NULL);
 
-    for (i = 0; i < len1 + len2; i++)
-        res[i] = 0;
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		n1 = num1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + res[i + j + 1] + carry;
+			carry = sum / 10;
+			res[i + j + 1] = sum % 10;
+		}
+		res[i + j + 1] += carry;
+	}
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        carry = 0;
-        n1 = num1[i] - '0';
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            n2 = num2[j] - '0';
-            sum = n1 * n2 + res[i + j + 1] + carry;
-            carry = sum / 10;
-            res[i + j + 1] = sum % 10;
-        }
-        res[i + j + 1] += carry;
-    }
+	start = 0;
+	while (start < (len1 + len2) && res[start] == 0)
+	{
+		start++;
+	}
 
-    start = 0;
-    while (start < len1 + len2 && res[start] == 0)
-        start++;
+	if (start == len1 + len2)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		for (; start < len1 + len2; start++)
+		{
+			_putchar(res[start] + '0');
+		}
+	}
 
-    if (start == len1 + len2)
-        _putchar('0');
-    else
-    {
-        for (; start < len1 + len2; start++)
-            _putchar(res[start] + '0');
-    }
-
-    _putchar('\n');
-    free(res);
+	_putchar('\n');
+	free(res);
 }
