c1dd48a73a5b7c78d2ad552de4d918f889dea67b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-08-29 15:23:25 +0400

    sds

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/* Adds two integers */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/* Subtracts two integers */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/* Multiplies two integers */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/* Divides two integers */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/* Modulo of two integers */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
