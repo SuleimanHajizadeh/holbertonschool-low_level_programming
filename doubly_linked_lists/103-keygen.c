7afde29fcb994f9bceb7aa3d159ebd2fd3b7a57a
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-12 09:04:08 +0000

    sdsd

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - generates a valid key for crackme5
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
 int main(int argc, char *argv[])
 {
     char key[100];
@@ -11,10 +18,9 @@ int main(int argc, char *argv[])
 
     len = strlen(argv[1]);
     for (i = 0; i < len; i++)
-        key[i] = argv[1][i] + len;  /* Example transformation */
+        key[i] = argv[1][i] + len;  /* transform username to key */
 
-    key[len] = '\0';
-
-    printf("%s", key);  /* NO newline */
+    key[len] = '\0';  /* NO newline */
+    printf("%s", key); /* NO newline */
     return 0;
 }
--- doubly_linked_lists/launch_test.sh
+++ doubly_linked_lists/launch_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# Launch crackme5 safely with the correct key
+
 username="$1"
+if [ -z "$username" ]; then
+    echo "Usage: $0 username"
+    exit 1
+fi
+
+# Run crackme5 with the generated key
 ./crackme5 "$username" "$(./keygen5 "$username")"
