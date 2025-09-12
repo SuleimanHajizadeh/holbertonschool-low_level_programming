e648492bc095ac8f6c55cec5c796a7c8955c8d6c
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-12 09:11:24 +0000

    sdsd

--- doubly_linked_lists/launch_test.sh
+++ doubly_linked_lists/launch_test.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Safe launch for crackme5
-
 username="$1"
-if [ -z "$username" ]; then
-    echo "Usage: $0 username"
-    exit 1
-fi
-
-# Run crackme5 using keygen output safely
-./crackme5 "$username" "$(./keygen5 "$username")
-"
+./crackme5 "$username" "$(./keygen5 "$username")"
