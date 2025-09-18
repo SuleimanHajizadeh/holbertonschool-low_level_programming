9a53ac1798a1734fbea03eeacdd7c1fa6cbcb0bb
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-18 19:53:20 +0000

    sdsd

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,27 +10,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    /* Allocate memory for the table structure */
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-        return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-    table->size = size;
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    /* Allocate memory for the array of pointers to hash_node_t */
-    table->array = malloc(sizeof(hash_node_t *) * size);
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
-    /* Initialize all pointers to NULL */
-    for (i = 0; i < size; i++)
-        table->array[i] = NULL;
-
-    return (table);
+	return (table);
 }
